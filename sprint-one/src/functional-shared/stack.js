cce70364964df8bfde28a22733c9e7840730d4ce
jwdomingo <johnwill.domingo@gmail.com>
2016-01-13 15:33:53 -0800

    Complete extend and methods.

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,26 @@
+var extend = function(obj){
+  for (var item in obj) {
+    for (var key in item) {
+      obj[key] = item[key];
+    }
+  }
+};
+
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newStack = {};
+  var storage = {};
+  var stackSize = 0;
+  return newStack;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push:  function(val){},
+  pop:   function(){},
+  size:  function(){
+    return stackSize;
+  }
+};
 
 
