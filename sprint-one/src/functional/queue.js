e46ac77906f29d619d40fdf78913f144d506e984
jwdomingo <johnwill.domingo@gmail.com>
2016-01-13 14:56:59 -0800

    Report 0 when queue empty and implement push method

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,5 +1,6 @@
 var Queue = function() {
   var someInstance = {};
+  var queueSize = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
@@ -7,12 +8,15 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[queueSize] = value;
+    queueSize++;
   };
 
   someInstance.dequeue = function() {
   };
 
   someInstance.size = function() {
+    return queueSize;
   };
 
   return someInstance;
