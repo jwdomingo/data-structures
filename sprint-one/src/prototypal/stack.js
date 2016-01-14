3e20d00eda5bf81ae29c947fc6982588c42b97d3
jwdomingo <johnwill.domingo@gmail.com>
2016-01-13 16:21:36 -0800

    Link stackMethods prototype to newStack class and implement size method

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,14 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newStack = Object.create(stackMethods);
+  newStack.stackSize = 0;
+  newStack.storage = {};
+  return newStack;
 };
 
-var stackMethods = {};
-
-
+var stackMethods = {
+  size: function() {
+    return this.stackSize;
+  }
+};
