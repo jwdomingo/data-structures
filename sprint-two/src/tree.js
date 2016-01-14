1d8444d097f9af9a15ca093addff6d4f8b6dc893
jwdomingo <johnwill.domingo@gmail.com>
2016-01-14 11:20:43 -0800

    Implement contains method using recursive closure function.

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -20,6 +20,25 @@ treeMethods.addChild = function(value) {
 };
 
 treeMethods.contains = function(target) {
+  var result = false;
+
+  var traverseTree = function(node){
+    if (node.value === target) {
+      result = true;
+      return;
+    }  
+    for (var child = 0; child < node.children.length; child++) {
+      if (node.children[child] === target) {
+        result = true;
+        return;
+      } else if (node.children.length > 0) {
+        traverseTree(node.children[child]);
+      }
+    }
+  };
+
+  traverseTree(this);
+  return result;
 };
 
 
