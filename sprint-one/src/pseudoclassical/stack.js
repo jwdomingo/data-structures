d195f7440d6cbcd97608a0be46b7131fa85725ca
jwdomingo <johnwill.domingo@gmail.com>
2016-01-13 21:09:46 -0800

    Add semicolons to method definitions.

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,19 +1,19 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-
+  
   this.storage = {};
   this.stackSize = 0;
 };
 
 Stack.prototype.size = function() {
   return this.stackSize;
-}
+};
 
 Stack.prototype.push = function(val) {
   this.storage[this.stackSize] = val;
   this.stackSize++;
-}
+};
 
 Stack.prototype.pop = function() {
   if (this.stackSize > 0) {
@@ -23,4 +23,4 @@ Stack.prototype.pop = function() {
 
     return aux;
   }
-}
+};
