2f517f0dedb1aa7906d415cd15e558ad20035d3f
jwdomingo <johnwill.domingo@gmail.com>
2016-01-13 16:30:38 -0800

    Link newQueue class to queueMethods prototype

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,6 +1,13 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var newQueue = Object.create(queueMethods);
+
+  newQueue.storage = {};
+  newQueue.queueSize = 0;
+
+  return newQueue;
 };
 
 var queueMethods = {};
