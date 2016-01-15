8ed40c4563061c7b3daecea190f7f5e9a6ab482c
jwdomingo <johnwill.domingo@gmail.com>
2016-01-14 20:04:47 -0800

    Implement all functions and storing values as literal key-value pairs in objects

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,19 +5,27 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(index, v);
+
+  if (this._storage.get(index) === undefined) {
+    var obj = {};
+    obj[k] = v;
+    this._storage.set(index, obj);
+  } else {
+    this._storage.get(index)[k] = v;
+  }
+
+  // this._storage.set(index, v);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(index);
+  return this._storage.get(index)[k];
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  delete this._storage.get(index)[k];
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
