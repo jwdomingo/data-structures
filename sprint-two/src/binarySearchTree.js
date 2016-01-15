d16b1be470bcf93fce0fe0bf4717dcd630134980
jwdomingo <johnwill.domingo@gmail.com>
2016-01-15 11:13:50 -0800

    Add insert, contains and depthFirstLog methods using functional instantiation pattern.

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,26 @@
 var BinarySearchTree = function(value) {
+  var newBST = {};
+  newBST.value = value;
+  newBST.left = null;
+  newBST.right = null;
+
+  newBST.insert = function (value){
+  // traverse tree recursively 
+     //start at value, go left or right
+  // if (.value < value) {
+
+  // }
+  };
+
+  newBST.contains = function (value){
+
+  };
+
+  newBST.depthFirstLog = function (value){
+
+  };
+
+  return newBST; 
 };
 
 
